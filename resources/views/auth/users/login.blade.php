f5d9f004758595220c67ce75b88c7e91731d0e03
russ <russelcuevas0@gmail.com>
2024-10-01 12:13:28 +0800

    designing-login-page

--- resources/views/auth/users/login.blade.php
+++ resources/views/auth/users/login.blade.php
@@ -3,17 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>UB - Assistments</title>
+    <link rel="stylesheet" href="{{ asset('auth/css/login.css') }}">
+    <link rel="shortcut icon" href="{{ asset('auth/images/ub-logo.png') }}" type="image/x-icon">
 </head>
 <body>
-    <h1>Login Page</h1>
-    <form action="{{ route('users.login.request') }}" method="POST">
-        @csrf
-        <label for="">Default ID</label>
-        <input type="text" name="default_id"><br>
-        <label for="">Password</label>
-        <input type="password" name="password"><br>
-        <input type="submit" value="Login">
-    </form>
+    <div class="form-container">
+        <div class="logo-title-wrapper">
+            <div class="logo">
+                <img src="{{ asset('auth/images/ub-logo.png') }}" alt="UB Logo">
+            </div>
+            <h1>Login Page</h1>
+        </div>
+
+        <!-- Error Message Section -->
+        @if(session('error'))
+            <div class="error-message">
+                {{ session('error') }}
+            </div>
+        @endif
+
+        <form action="{{ route('users.login.request') }}" method="POST">
+            @csrf
+            <label style="font-weight: 600;" for="default_id">Default ID</label>
+            <input type="text" name="default_id" id="default_id" required>
+
+            <label style="font-weight: 600;" for="password">Password</label>
+            <input type="password" name="password" id="password" required>
+            <button type="submit">Login</button>
+        </form>
+    </div>
 </body>
 </html>
