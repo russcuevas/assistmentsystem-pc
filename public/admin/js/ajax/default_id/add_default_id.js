020b2eee357ca6df1f06e6cea338840a952be12d
russ <russelcuevas0@gmail.com>
2024-10-08 13:30:36 +0800

    ui

--- public/admin/js/ajax/default_id/add_default_id.js
+++ public/admin/js/ajax/default_id/add_default_id.js
@@ -1,38 +1,65 @@
-$('.addDefaultId').on('submit', function (event) {
+$('.addDefaultId').validate({
+    highlight: function (input) {
+        $(input).parents('.form-line').addClass('error');
+    },
+    unhighlight: function (input) {
+        $(input).parents('.form-line').removeClass('error');
+    },
+    errorPlacement: function (error, element) {
+        $(element).parents('.form-group').append(error);
+    },
+    rules: {
+        count: {
+            required: true,
+            min: 1
+        }
+    },
+    messages: {
+        count: {
+            required: "Please enter the number of IDs to add",
+            min: "Please enter a value greater than or equal to 1"
+        }
+    }
+});
+
+$('form.addDefaultId').on('submit', function (event) {
     event.preventDefault();
 
     const form = $(this);
-    const formData = form.serialize();
-    const addDefaultIdUrl = form.data('route-add-default-id');
-    addShowLoading();
+    
+    if (form.valid()) {
+        const formData = form.serialize();
+        const addDefaultIdUrl = form.data('route-add-default-id');
+        addShowLoading();
 
-    $.ajax({
-        url: addDefaultIdUrl,
-        type: 'POST',
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        data: formData,
-        success: function (response) {
-            swal({
-                title: "Success!",
-                text: response.success,
-                icon: "success",
-            }).then(() => {
-                location.reload();
-            });
-        },
-        error: function (xhr) {
-            const errors = xhr.responseJSON.errors;
-            let errorMessage = 'An error occurred:';
-            if (errors) {
-                $.each(errors, function (key, value) {
-                    errorMessage += `\n- ${value[0]}`;
+        $.ajax({
+            url: addDefaultIdUrl,
+            type: 'POST',
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            data: formData,
+            success: function (response) {
+                swal({
+                    title: "Success!",
+                    text: response.success,
+                    icon: "success",
+                }).then(() => {
+                    location.reload();
                 });
+            },
+            error: function (xhr) {
+                const errors = xhr.responseJSON.errors;
+                let errorMessage = 'An error occurred:';
+                if (errors) {
+                    $.each(errors, function (key, value) {
+                        errorMessage += `\n- ${value[0]}`;
+                    });
+                }
+                swal("Error!", errorMessage, "error");
             }
-            swal("Error!", errorMessage, "error");
-        }
-    });
+        });
+    }
 });
 
 function addShowLoading() {
--- resources/views/admin/default_id/default_id.blade.php
+++ resources/views/admin/default_id/default_id.blade.php
@@ -212,6 +212,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Custom Js -->
+    <script src="{{ asset('admin/js/pages/forms/form-validation.js') }}"></script>
     <script src="{{ asset('admin/js/admin.js') }}"></script>
     <script src="{{ asset('admin/js/pages/tables/jquery-datatable.js') }}"></script>
     <script src="{{ asset('admin/js/HoldOn.js') }}"></script>
